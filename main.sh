8b8173b27918e8e6576d5401ca38dc90d4411770
Shane <ccniubi@163.com>
2018-01-23 09:51:27 +0800

    update for jd cloud image

--- main.sh
+++ main.sh
@@ -4,9 +4,12 @@ set -x
 cd /root/script
 /root/script/auto_disk.sh
 /root/script/init.sh
-/root/script/tomcat_install.sh 1.6 6
+/root/script/tomcat_install.sh 1.6 7
+#yes|cp 1.6CE/*.jar /export/servers/jdk/jre/lib/security
 sleep 2
 /root/script/nginx_install.sh
+yum install -y unzip&&wget http://pocteam-obj.oss.cn-north-1.jcloudcs.com/sps-agent-flume.zip && unzip -d /export/servers/sps-agent-flume sps-agent-flume.zip && rm -f sps-agent-flume.zip
 chown -R admin:admin /export
+wget devops.oss.cn-south-1.jcloudcs.com/ifrit/ifrit-agent-installer-v0.01.348.4440aa4.20171205134020.bin -O installer && sh installer /export/servers/ifrit znkf && rm -f installer
 sed -i '/main.sh/d' /etc/rc.local
 rm -rf /root/script
